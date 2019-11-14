e6c9c8eac24bf8f0cad56ef6f490fc1376c309fc
Bardur Arantsson <bardur@scientician.net>
2019-11-14 22:13:26 +0100

    Bump to ScalaTest 3.0.8
    This version is compatible with Scala 2.13.x.

--- project/Version.scala
+++ project/Version.scala
@@ -2,7 +2,7 @@ object Version {
 
   val log4j = "2.8.2"
   val postgresqlDriver = "42.1.4.jre7"
-  val scalaTest = "3.0.4"
+  val scalaTest = "3.0.8"
   val scalikeJDBC = "3.2.2"
   val tempgresClient = "1.1.0"
 
