97fb3594d8d2bfabaedd24d09c78bd2668d02b3c
Bardur Arantsson <bardur@scientician.net>
2019-11-14 22:16:14 +0100

    Bump to ScalikeJDBC 3.4.0
    This is a test-only dependency, so it does not affect binary
    compatibility.

--- project/Version.scala
+++ project/Version.scala
@@ -3,7 +3,7 @@ object Version {
   val log4j = "2.8.2"
   val postgresqlDriver = "42.1.4.jre7"
   val scalaTest = "3.0.8"
-  val scalikeJDBC = "3.2.2"
+  val scalikeJDBC = "3.4.0"
   val tempgresClient = "1.1.0"
 
 }
